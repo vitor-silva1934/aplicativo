5f56e227d0227bcc1b6db4694c00e4d4e8762c4a
Vitor Hugo Conceição Silva <16960535794@pms.rj.gov.br>
2024-11-12 13:26:40 -0300

    adicionado barra de pesqu

--- produtos/views.py
+++ produtos/views.py
@@ -61,9 +61,14 @@ def criar_produto(request):
 
 
 def deletar_produto(request, produto_id):
-    produto = Produto.objects.get(id=produto_id)
-    produto.delete()
-    return redirect('listar_produtos')
+    produto = get_object_or_404(Produto, id=produto_id)
+
+    if request.method == 'POST': 
+        produto.delete()
+        return redirect('listar_produtos')
+
+    context = {'produto': produto}  #Passa as informações do produto para o template de confirmação
+    return render(request, 'produtos/confirm_produto_delete.html', context)
 
 
 def detalhes_produto(request, id):
